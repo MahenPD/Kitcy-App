6ead0819502ecbf32360213696fea5796712b734
Fazna Fouseen <faznafouseen@Faznas-MacBook-Pro.local>
2022-01-15 17:12:07 +0530

    Add intro popups, Game popups

--- Fazna/popup/popups.js
+++ Fazna/popup/popups.js
@@ -4,25 +4,29 @@ function openPopup(){
 
   }
 
-  function openToastiPhone(message){
-
-	$("#popupBasic").empty();
-	
-	var str = `<span> ${message} <a href="#" data-rel="back" class="toast-close">OK</a> </span>`;
-        myClone1 = $(str);      
-        myClone1.appendTo("#popupBasic").trigger('create');
-
-	$( "#popupBasic" ).popup( "open", {positionTo: "#iphone-area", transition: "pop"});
-
-	setTimeout(() => {
-	  // Invoke the close() method after 3seconds
-	  $( "#popupBasic" ).popup( "close");
-	}, 3000);
-  }
 
   function popupConfirm(){
     $( "#popup-remove" ).popup( "close", {transition: "pop"});
+    $( "#popup-redeem-confirm" ).popup( "close", {transition: "pop"});
+  }
 
+
+  function nextslide1(){
+    $( "#popup-slide-title" ).text("Cooking Safe Food");
+    $( "#popup-slide-content").text("We maintain safety and keep clean while making your food")
+    $( "#popup-slide-img").attr("src","assets/review.png")
+    $( "#popup-slide-btn").attr("onClick","nextslide2()")
+  }
+
+  function nextslide2(){
+    $( "#popup-slide-title" ).text("Quick Delivery");
+    $( "#popup-slide-content").text("Bringing you your favorite foods at the quickest time possible")
+    $( "#popup-slide-img").attr("src","assets/food-delivery.png")
+    $( "#popup-slide-btn").attr("onClick","nextslide3()")
+  }
+
+  function nextslide3(){
+    $( "#popup-area" ).popup( "close");
   }
 
   
